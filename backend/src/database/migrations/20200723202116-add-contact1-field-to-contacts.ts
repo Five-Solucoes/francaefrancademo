36bf2527711f2ce8d49d7550dcab84475ede1a3a
Marcos Santiago <santiago@ffxsistemas.com>
2023-10-23 14:11:20 -0300

    new

---
+++ backend/src/database/migrations/20200723202116-add-contact1-field-to-contacts.ts
@@ -0,0 +1,15 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.addColumn("Contacts", "coluna_geral_1", {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: ""
+    });
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.removeColumn("Contacts", "coluna_geral_1");
+  }
+};
