36df6bc7c3f346606bf74179b85047c109fe1d4c
Marcos Santiago <santiago@ffxsistemas.com>
2023-10-23 13:56:37 -0300

    fixes

--- backend/src/database/migrations/20200723202116-add-contactscustom-field-to-contacts.ts
+++ backend/src/database/migrations/20200723202116-add-contactscustom-field-to-contacts.ts
@@ -2,63 +2,73 @@
 import { QueryInterface, DataTypes } from "sequelize";
 
 module.exports = {
-  up: (queryInterface: QueryInterface) => {
-    return queryInterface.addColumn("Contacts", {
-      coluna_geral_1: {
+  up: function (queryInterface, Sequelize) {
+    return [
+      queryInterface.addColumn('Contacts', 'coluna_geral_1', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_2: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_2', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_3: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_3', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_4: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_4', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_5: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_5', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_6: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_6', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_7: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_7', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_8: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_8', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_9: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_9', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-      coluna_geral_10: {
+      }),
+      queryInterface.addColumn('Contacts', 'coluna_geral_10', {
         type: DataTypes.STRING,
         allowNull: false,
         defaultValue: ""
-      },
-    });
+      })
+    ];
   },
 
-
-  down: (queryInterface: QueryInterface) => {
-    return queryInterface.removeColumn("Contacts", "email");
+  down: function (queryInterface, Sequelize) {
+    return [
+      queryInterface.removeColumn('Contacts', 'coluna_geral_1'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_2'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_3'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_4'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_5'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_6'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_7'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_8'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_9'),
+      queryInterface.removeColumn('Contacts', 'coluna_geral_10'),
+    ];
   }
 };
